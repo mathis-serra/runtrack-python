38b1febbe2972500ca7b9c93b85c6a2886cae891
mathis-serra <mathis.serra@laplateforme.io>
2023-11-20 14:45:30 +0100

    add 4 stars

--- jour1/JOB05/main.py
+++ jour1/JOB05/main.py
@@ -1,3 +1,10 @@
-ma_string = "a b c d e f g h i j k l m n o p q r s t u v w x y z"
-reversed_string = ma_string[::-1]
+my_string = "a b c d e f g h i j k l m n o p q r s t u v w x y z"
+
+my_list = list(my_string)
+
+for i in range(len(my_list)//2):
+    my_list[i], my_list[-i-1] = my_list[-i-1], my_list[i]
+
+reversed_string = ''.join(my_list)
+
 print(reversed_string)
