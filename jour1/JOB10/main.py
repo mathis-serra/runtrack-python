eab1021fc91701b24853c92c4d55b518d3b901aa
mathis-serra <mathis.serra@laplateforme.io>
2023-11-20 15:26:05 +0100

    fin  de job

--- jour1/JOB10/main.py
+++ jour1/JOB10/main.py
@@ -1,19 +1,19 @@
 investisement_initial = 10000
-rendement_annuel = 10
+rendement_annuel = 0.1
 
-gain_annuel = investisement_initial * rendement_annuel / 100
+gain_annuel = investisement_initial * rendement_annuel 
 print("Gain annuel initial:", gain_annuel)
 
 investisement_initial += 5000
-rendement_annuel += 2
+rendement_annuel += 0.02
 
-gain_annuel = investisement_initial * rendement_annuel / 100
+gain_annuel = investisement_initial * rendement_annuel 
 print("Nouveau Gain annuel:", gain_annuel)
 
 montant_retiré = investisement_initial * 0.1
 investisement_initial -= montant_retiré 
 
-rendement_annuel -= 1
+rendement_annuel -= 0.01
 
-gain_annuel = investisement_initial * rendement_annuel / 100
+gain_annuel = investisement_initial * rendement_annuel 
 print("Gain annuel final:", gain_annuel)
